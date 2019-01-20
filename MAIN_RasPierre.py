9cc4439c1a488c33e54afcdd16521950cb365b8b
Sawy89 <ddeennyy@hotmail.it>
2019-01-20 18:15:03 +0100

    login OK nel main

--- MAIN_RasPierre.py
+++ MAIN_RasPierre.py
@@ -7,37 +7,40 @@ Contiene il Main del progetto RasPierre
 @author: ddeen
 """
 
-from sqlalchemy import create_engine
-import Piscina
-from Piscina import Base
-from sqlalchemy.orm import sessionmaker
-from Piscina import *
-from flask import Flask
-
-# ??
-SECRET_KEY = '42o108'
+#from sqlalchemy import create_engine
+#import Piscina
+from Piscina import Base, piscina_flask
+#from sqlalchemy.orm import sessionmaker
+#from Piscina import *
+from flask import Flask, flash, render_template, request, session
+import os
 
 app = Flask(__name__)
 app.register_blueprint(piscina_flask, url_prefix='/piscina')
-app.secret_key = SECRET_KEY
+
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('index.html')
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'elenabella22' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+    
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
-
-
-
-
-#%% MAIN Piscina
-#if __name__ == '__main__':
-
-# Creo DB engine
-#engine = create_engine('sqlite:///sport.db')
-
-# Creo il DB
-#Base.metadata.create_all(engine)
+ 
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+#    app.debug = 5000
+    app.run(debug=True,host='0.0.0.0', port=5000)
